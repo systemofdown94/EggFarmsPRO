e7803ab6048fa0df00a36d977914b7c5e0a6e525
esplusnin <esplusnin94@yandex.ru>
2026-02-27 07:38:58 +0300

    Initial Commit

---
+++ Chicken_manager/Chicken_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chicken_managerApp.swift
+//  Chicken_manager
+//
+//  Created by Евгений on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Chicken_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chicken_manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chicken_manager
+//
+//  Created by Евгений on 27.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
